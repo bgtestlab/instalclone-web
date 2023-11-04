4ee06e5e933b7b70a441ac24ff85099533ee3697
codeflow <boramismn@gmail.com>
2023-11-04 11:56:11 +0900

    9.6 React Variables

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import NotFound from "./screens/NotFound";
+import { useReactiveVar } from "@apollo/client";
+import { isLoggedInVar } from "./apollo";
+
 function App() {
-  const isLoggedIn = true;
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
   return (
     <Router>
       <Switch>
